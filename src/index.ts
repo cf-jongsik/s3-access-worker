bb25e2598067b9cb85096ac1d32eba231d98e566
Jongsik <jongsik@cloudflare.com>
2025-02-25 19:50:05 +0900

    updated: library added: cache API changed: rewrote logics

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,38 @@ import { S3Client, GetObjectCommand, GetObjectCommandInput } from '@aws-sdk/clie
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext) {
-		ctx.passThroughOnException();
+		let cache = caches.default;
+		const cached = await cache.match(request);
+
+		if (cached) {
+			if (env.DEBUG) console.debug('Cache hit');
+			return cached;
+		}
+		if (env.DEBUG) console.debug('Cache miss');
+
 		const url = new URL(request.url);
 		const key = url.pathname.slice(1);
 
+		if (env.DEBUG) console.debug('Key:', key);
+
+		const { S3_ACCESS_ID, S3_SECRET_KEY, S3_BUCKET_NAME, S3_BUCKET_REGION } = env;
+		if (!S3_ACCESS_ID || !S3_SECRET_KEY) {
+			return new Response('ACCESS ID/KEY are required', { status: 500 });
+		}
+		if (!S3_BUCKET_NAME) {
+			return new Response('Bucket name is required', { status: 500 });
+		}
+		if (!S3_BUCKET_REGION) {
+			return new Response('Bucket region is required', { status: 500 });
+		}
+
 		const client = new S3Client({
-			bucketEndpoint: true,
-			region: 'auto',
-			credentials: { accessKeyId: env.accessKeyId, secretAccessKey: env.secretAccessKey },
+			region: S3_BUCKET_REGION,
+			credentials: { accessKeyId: S3_ACCESS_ID, secretAccessKey: S3_SECRET_KEY },
 		});
 
 		const params = {
-			Bucket: env.bucketURL,
+			Bucket: S3_BUCKET_NAME,
 			Key: key || 'index.html',
 		} satisfies GetObjectCommandInput;
 
@@ -21,7 +41,7 @@ export default {
 
 		try {
 			const data = await client.send(command);
-			if (!data) return Response.redirect(url.origin, 302);
+			if (!data) return new Response('', { status: 404, statusText: 'Not Found' });
 			const headers = new Headers();
 			if (data.ContentType) {
 				headers.set('content-type', data.ContentType);
@@ -41,9 +61,13 @@ export default {
 			if (data.ContentLanguage) {
 				headers.set('content-language', data.ContentLanguage);
 			}
-			return new Response(data.Body?.transformToWebStream(), { headers });
+			headers.append('Cache-Control', 's-maxage=3600');
+			const response = new Response(data.Body?.transformToWebStream(), { headers });
+			ctx.waitUntil(cache.put(request, response.clone()));
+			return response;
 		} catch (error) {
-			return Response.redirect(url.origin, 302);
+			console.error(error);
+			return new Response('Internal Server Error', { status: 500, statusText: 'Internal Server Error' });
 		}
 	},
 };
